de95fa36d03a3dce6ea18483b4920b6351466e3d
Nazariy Borbiuk <nbo@jooble.com>
2020-04-16 22:44:37 +0300

    Add Cathcer empty console project

---
+++ Catcher/Catcher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Catcher
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var catcher = new СatcherService();
+
+			try
+			{
+				catcher.Run();
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+
+			Console.WriteLine("");
+		}
+	}
+}
---
+++ Catcher/Catcher/СatcherService.cs
@@ -0,0 +1,12 @@
+﻿namespace Catcher
+{
+	public class СatcherService
+	{
+		public СatcherService()
+		{
+
+		}
+
+		public void Run() { }
+	}
+}
